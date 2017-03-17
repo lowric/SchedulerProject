554028dc31a79463da03542045fe2dbf131fb4a6
Eric Watat Lowe <watatlowe@gmail.com>
2017-03-17 09:57:15 -0400

    creating new repository
    new project

---
+++ src/schedule/ScheduleMaker.java
@@ -0,0 +1,10 @@
+package schedule;
+
+public class ScheduleMaker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/schedule/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author ericwatatlowe
+ *
+ */
+package schedule;
